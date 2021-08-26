b1895f8dc324ad409214ef3b7947e1e6fd4ee30e
Avik <1828008@kiit.ac.in>
2021-08-27 02:55:57 +0530

    Completed AC, AI Speaker & Washing Machine Pages

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_restful import Api, Resource
 from datetime import timedelta
 from gevent.pywsgi import WSGIServer # WSGI Server
 
+import writeCSV as write    #Contains the Methods for writing into CSV Files
 
 app = Flask(__name__)
 app.secret_key = "hello"
@@ -13,70 +14,100 @@ api = Api(app)
 app.debug = True
 
 
+
+# === These routes load the respective HTML Pages === #
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
+@app.route('/test', methods=['GET'])
+def test():
+    return render_template('test.html')
 
 @app.route('/washing-machine', methods=['GET'])
 def washingMachine():
     return render_template('washingMachine.html')
 
-@app.route('/download', methods=['GET'])
-def downloadFile ():
-    path = "nameList.csv"
+@app.route('/air-conditioner', methods=['GET'])
+def airConditioner():
+    return render_template('AC.html')
+
+@app.route('/ai-speaker', methods=['GET'])
+def aiSpeaker():
+    return render_template('AiSpeaker.html')
+
+@app.route('/family-hub', methods=['GET'])
+def familyHub():
+    return render_template('FamilyHub.html')
+
+@app.route('/microwave', methods=['GET'])
+def microWave():
+    return render_template('Microwave.html')
+
+@app.route('/mobile', methods=['GET'])
+def mobile():
+    return render_template('Mobile.html')
+
+@app.route('/watch', methods=['GET'])
+def watch():
+    return render_template('watch.html')
+
+
+
+# === These routes download the respective CSV files of each item  === #
+
+@app.route('/download-washing-machine', methods=['GET'])
+def download_washingmachine ():
+    path = "washing-machine.csv"
+    return send_file(path, as_attachment=True)
+
+@app.route('/download-air-conditioner', methods=['GET'])
+def download_airconditioner ():
+    path = "air-conditioner.csv"
+    return send_file(path, as_attachment=True)
+
+@app.route('/download-ai-speaker', methods=['GET'])
+def download_aispeaker ():
+    path = "ai-speaker.csv"
+    return send_file(path, as_attachment=True)
+
+@app.route('/download-family-hub', methods=['GET'])
+def downloadFile_familyhub ():
+    path = "family-hub.csv"
+    return send_file(path, as_attachment=True)
+
+@app.route('/download-microwave', methods=['GET'])
+def downloadFile_microwave ():
+    path = "microwave.csv"
+    return send_file(path, as_attachment=True)
+
+@app.route('/download-mobile', methods=['GET'])
+def downloadFile_mobile ():
+    path = "mobile.csv"
+    return send_file(path, as_attachment=True)
+
+@app.route('/download-watch', methods=['GET'])
+def downloadFile_watch ():
+    path = "watch.csv"
     return send_file(path, as_attachment=True)
 
 
-class Save_CSV(Resource):
+# === These Function Definitions are in the "writeCSV.py" file === #
 
-    def post(self):
-
-        _powerRating = request.form.get('power')
-        _type = request.form.get('type')
-
-        _model = request.form.get('model')
-        _capacity = request.form.get('capacity')
-        _bodyColor = request.form.get('body-color')
-        _panelDisplay = request.form.get('panel-display')
-
-        _specialFeature = request.form.get('sp-feature')
-
-        _connectionType = request.form.get('connection-type')
-        _displayLanguage = request.form.get('display-lang')
-        _speechLanguage = request.form.get('speech-lang')
-
-        _timestamp = datetime.datetime.now()
-
-
-        fieldnames = ['Power Rating', 'Type', 'Model' , 'Capacity' , 'Body Color' , 'Panel Display' , 'Special Feature' , 'Connection Type' , 'Display Language' , 'Speech Language' , 'Timestamp']
-
-        with open('nameList.csv', 'a') as inFile:
-            writer = csv.DictWriter(inFile, fieldnames=fieldnames)
-            writer.writerow(
-                {'Power Rating': _powerRating, 
-                 'Type': _type,
-                 'Model': _model,
-                 'Capacity': _capacity,
-                 'Body Color': _bodyColor,
-                 'Panel Display': _panelDisplay,
-                 'Special Feature': _specialFeature,
-                 'Connection Type': _connectionType,
-                 'Display Language': _displayLanguage,
-                 'Speech Language': _speechLanguage,
-                 'Timestamp': _timestamp
-                 })
-
-        return redirect(url_for('washingMachine'))
-
-
-api.add_resource(Save_CSV, "/save")
+api.add_resource(write.Save_WashingMachine_CSV, "/save-washing-machine")
+api.add_resource(write.Save_AirConditioner_CSV, "/save-air-conditioner")
+api.add_resource(write.Save_AiSpeaker_CSV, "/save-ai-speaker")
+api.add_resource(write.Save_FamilyHub_CSV, "/save-family-hub")
+api.add_resource(write.Save_Microwave_CSV, "/save-microwave")
+api.add_resource(write.Save_Mobile_CSV, "/save-mobile")
+api.add_resource(write.Save_Watch_CSV, "/save-watch")
 
 
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000)) 
+    port = int(os.environ.get('PORT', 5000)) # Application Runs on 5000 PORT
     http_server = WSGIServer(('0.0.0.0', port), app)
     print(f"Flask Server Started at {str(port)}")
     http_server.serve_forever()
---
+++ writeCSV.py
@@ -0,0 +1,229 @@
+# ================================================ #
+# This file contains the methods to save the Form Data into the CSV files.
+# Each Class corresponds to a particular item.
+# The form details are fetched from the HTML file and are saved in a CSV File.
+# ================================================ #
+
+import datetime, os, csv
+
+from flask import Flask, redirect, url_for, request, render_template, redirect, send_file
+from flask_restful import Resource
+from datetime import timedelta
+
+
+# === Washing Machine === #
+class Save_WashingMachine_CSV(Resource):
+
+    def post(self):
+
+        _powerRating = request.form.get('power')
+        _type = request.form.get('type')
+
+        _model = request.form.get('model')
+        _capacity = request.form.get('capacity')
+        _bodyColor = request.form.get('body-color')
+        _panelDisplay = request.form.get('panel-display')
+
+        _specialFeature = request.form.get('sp-feature')
+
+        _connectionType = request.form.get('connection-type')
+        _displayLanguage = request.form.get('display-lang')
+        _speechLanguage = request.form.get('speech-lang')
+
+        _timestamp = datetime.datetime.now()
+
+
+        fieldnames = ['Power Rating', 'Type', 'Model' , 'Capacity' , 'Body Color' , 'Panel Display' , 'Special Feature' , 'Connection Type' , 'Display Language' , 'Speech Language' , 'Timestamp']
+
+        with open('washing-machine.csv', 'a') as inFile:
+            writer = csv.DictWriter(inFile, fieldnames=fieldnames)
+            writer.writerow(
+                {'Power Rating': _powerRating, 
+                 'Type': _type,
+                 'Model': _model,
+                 'Capacity': _capacity,
+                 'Body Color': _bodyColor,
+                 'Panel Display': _panelDisplay,
+                 'Special Feature': _specialFeature,
+                 'Connection Type': _connectionType,
+                 'Display Language': _displayLanguage,
+                 'Speech Language': _speechLanguage,
+                 'Timestamp': _timestamp
+                 })
+
+        return redirect(url_for('washingMachine'))
+
+
+
+# === Air Conditioner === #
+class Save_AirConditioner_CSV(Resource):
+
+    def post(self):
+
+        _powerRating = request.form.get('power')
+        _type = request.form.get('type')
+
+        _model = request.form.get('model')
+        _capacity = request.form.get('capacity')
+        _bodyColor = request.form.get('body-color')
+
+        _specialFeature = request.form.get('sp-feature')
+
+        _connectionType = request.form.get('connection-type')
+        _displayLanguage = request.form.get('display-lang')
+        _speechLanguage = request.form.get('speech-lang')
+
+        _timestamp = datetime.datetime.now()
+
+
+        fieldnames = ['Power Rating', 'Type', 'Model' , 'Capacity' , 'Body Color' , 'Special Feature' , 'Connection Type' , 'Display Language' , 'Speech Language' , 'Timestamp']
+
+        with open('air-conditioner.csv', 'a') as inFile:
+            writer = csv.DictWriter(inFile, fieldnames=fieldnames)
+            writer.writerow(
+                {'Power Rating': _powerRating, 
+                 'Type': _type,
+                 'Model': _model,
+                 'Capacity': _capacity,
+                 'Body Color': _bodyColor,
+                 'Special Feature': _specialFeature,
+                 'Connection Type': _connectionType,
+                 'Display Language': _displayLanguage,
+                 'Speech Language': _speechLanguage,
+                 'Timestamp': _timestamp
+                 })
+
+        return redirect(url_for('airConditioner'))
+
+
+# === AI Speaker === #
+class Save_AiSpeaker_CSV(Resource):
+
+    def post(self):
+
+        _farField = request.form.get('far-field')
+        _bodyColor = request.form.get('body-color')
+
+
+        _connectionType = request.form.get('connection-type')
+        _displayLanguage = request.form.get('display-lang')
+        _speechLanguage = request.form.get('speech-lang')
+
+        _currentMode = request.form.get('current-mode')
+
+        _timestamp = datetime.datetime.now()
+
+
+        fieldnames = ['Far Field Voice Processing', 'Body Color' , 'Connection Type' , 'Display Language' , 'Speech Language' , 'Current Mode' ,'Timestamp']
+
+        with open('ai-speaker.csv', 'a') as inFile:
+            writer = csv.DictWriter(inFile, fieldnames=fieldnames)
+            writer.writerow(
+                {'Far Field Voice Processing': _farField, 
+                 'Body Color': _bodyColor,
+                 'Connection Type': _connectionType,
+                 'Display Language': _displayLanguage,
+                 'Speech Language': _speechLanguage,
+                 'Current Mode':_currentMode,
+                 'Timestamp': _timestamp
+                 })
+
+        return redirect(url_for('aiSpeaker'))
+
+
+# === Watch === #
+class Save_Watch_CSV(Resource):
+
+    def post(self):
+       
+       ##==================================
+       # Pull the form data into here & push it into the CSV.
+       ##==================================
+
+        _timestamp = datetime.datetime.now()
+
+
+        fieldnames = ['Timestamp']
+
+        with open('watch.csv', 'a') as inFile:
+            writer = csv.DictWriter(inFile, fieldnames=fieldnames)
+            writer.writerow(
+                {
+                 'Timestamp': _timestamp
+                 })
+
+        return redirect(url_for('watch'))
+
+
+# === Mobile === #
+class Save_Mobile_CSV(Resource):
+
+    def post(self):
+
+         ##==================================
+         # Pull the form data into here & push it into the CSV.
+         ##==================================
+
+        _timestamp = datetime.datetime.now()
+
+
+        fieldnames = ['Timestamp']
+
+        with open('mobile.csv', 'a') as inFile:
+            writer = csv.DictWriter(inFile, fieldnames=fieldnames)
+            writer.writerow(
+                {
+                 'Timestamp': _timestamp
+                 })
+
+        return redirect(url_for('mobile'))
+
+
+# === Microwave === #
+class Save_Microwave_CSV(Resource):
+
+    def post(self):
+
+            ##==================================
+            # Pull the form data into here & push it into the CSV.
+            ##==================================
+
+        _timestamp = datetime.datetime.now()
+
+
+        fieldnames = ['Timestamp']
+
+        with open('microwave.csv', 'a') as inFile:
+            writer = csv.DictWriter(inFile, fieldnames=fieldnames)
+            writer.writerow(
+                {
+                 'Timestamp': _timestamp
+                 })
+
+        return redirect(url_for('microwave'))
+
+
+# === FamilyHub === #
+class Save_FamilyHub_CSV(Resource):
+
+    def post(self):
+
+        ##==================================
+        # Pull the form data into here & push it into the CSV.
+        ##==================================
+
+        _timestamp = datetime.datetime.now()
+
+
+        fieldnames = ['Timestamp']
+
+        with open('family-hub.csv', 'a') as inFile:
+            writer = csv.DictWriter(inFile, fieldnames=fieldnames)
+            writer.writerow(
+                {
+                 'Timestamp': _timestamp
+                 })
+
+        return redirect(url_for('familyHub'))
+
+
