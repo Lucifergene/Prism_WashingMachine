f9b369bc4189e7746c9af77e73135b762818089a
himanshu.hi <himanshu.hi@highradius.com>
2021-08-29 14:48:44 +0530

    Updated Family Hub

--- writeCSV.py
+++ writeCSV.py
@@ -208,20 +208,98 @@ class Save_FamilyHub_CSV(Resource):
 
     def post(self):
 
-        ##==================================
-        # Pull the form data into here & push it into the CSV.
-        ##==================================
+        _type = request.form.get('type')
+        _powerRating = request.form.get('power')
+
+        _model = request.form.get('model')
+        _capacity = request.form.get('capacity')
+        _dimWidth = request.form.get('dimWidth')
+        _dimDepth = request.form.get('dimDepth')
+        _weight = request.form.get('weight')
+        _bodyColor = request.form.get('body-color')
+
+        _familyHub = request.form.get('familyHub')
+        _viewInside = request.form.get('viewInside')
+        _errorCodes = request.form.get('error-codes-software')
+
+        _smartThingSupport = request.form.get('smartThingSupport')
+        _bluetooth = request.form.get('bluetooth')
+        _wifi = request.form.get('wifi')
+        _voiceAssistance = request.form.get('voiceAssistanct')
+        _displayLangConn = request.form.get('display-lang-conn')
+        _speechLangConn = request.form.get('speech-lang-conn')
+
+        _currentMode = request.form.get('current-mode')
+        _previousMode = request.form.get('previous-mode')
+        _errorState = request.form.get('error-state')
+        _lastOperation = request.form.get('last-operation')
+        _currentOperatingTime = request.form.get('CurrOpr')
+        _wifiConnected = request.form.get('wifiConnected')
+        _bluetoothConnected = request.form.get('bluetoothConnected')
+        _internetAvailable = request.form.get('internetAvailable')
+        _displayLang = request.form.get('display-lang')
+        _speechLang = request.form.get('speech-lang')
 
         _timestamp = datetime.datetime.now()
 
-
-        fieldnames = ['Timestamp']
+        fieldnames = [
+            'Type', 'Power Rating', 'Model', 'Capacity', 'Width', 'Depth',
+            'Weight',
+            'Body Color',
+            'Family Hug',
+            'View Inside',
+            'Error Codes',
+            'Smart Things Support',
+            'Bluetooth',
+            'Wifi',
+            'Voice Assistance',
+            'Display Lang Connnection',
+            'Speech Lang Connection',
+            'Current Mode',
+            'Previous Mode',
+            'Error State',
+            'Last Operation',
+            'Current Operating Time',
+            'Wifi Connected',
+            'Bluetooth Connected',
+            'Internet Available',
+            'Display Lang OS',
+            'Speech Lang OS',
+            'Timestamp'
+        ]
 
         with open('family-hub.csv', 'a') as inFile:
             writer = csv.DictWriter(inFile, fieldnames=fieldnames)
             writer.writerow(
                 {
-                 'Timestamp': _timestamp
+                    'Type': _type
+                    'Power Rating': _powerRating
+                    'Model': _model
+                    'Capacity': _capacity
+                    'Width': _dimWidth
+                    'Depth': _dimDepth
+                    'Weight': _weight
+                    'Body Color': _bodyColor
+                    'Family Hug': _familyHub
+                    'View Inside': _viewInside
+                    'Error Codes': _errorCodes
+                    'Smart Things Support': _smartThingSupport
+                    'Bluetooth': _bluetooth
+                    'Wifi': _wifi
+                    'Voice Assistance': _voiceAssistance
+                    'Display Lang Connnection': _displayLangConn
+                    'Speech Lang Connection': _speechLangConn
+                    'Current Mode': _currentMode
+                    'Previous Mode': _previousMode
+                    'Error State': _errorState
+                    'Last Operation': _lastOperation
+                    'Current Operating Time': _currentOperatingTime
+                    'Wifi Connected': _wifiConnected
+                    'Bluetooth Connected': _bluetoothConnected
+                    'Internet Available': _internetAvailable
+                    'Display Lang OS': _displayLang
+                    'Speech Lang OS': _speechLang
+                    'Timestamp': _timestamp
                  })
 
         return redirect(url_for('familyHub'))
